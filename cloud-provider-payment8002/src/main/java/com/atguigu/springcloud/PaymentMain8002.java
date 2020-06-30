c8ad21a5a8286e34f9e14dd0011965f3f0d050d2
shilin <shilin@jingkunsystem.com>
2020-06-30 11:41:31 +0800

    启动类名修改8001——> 8002

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -11,9 +11,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class PaymentMain8001 {
+public class PaymentMain8002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(PaymentMain8002.class,args);
         System.out.println("======payment8002启动成功======");
     }
 }
